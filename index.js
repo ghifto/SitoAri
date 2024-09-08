ab6b9e555c8f35ba41d4ba23a97701ddf1d92579
Nicolò Migliorini <69370563+ghifto@users.noreply.github.com>
2024-09-08 14:51:49 +0200

    add Node JS

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();
 const DataStore = require('nedb');
 
+app.listen(3000, () => console.log('listening on port 3000'));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/api', (request, response) => {
-    console.log(request.body);
-});
 
 const database = new DataStore('data/database.db');
 database.loadDatabase();
 
-
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    response.json({
+        status: "success"
+    });
+});
 
 const timestamp = Date.now();
