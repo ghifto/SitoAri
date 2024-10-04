62a0902f45f6d3cd404dd742d0df168a13da7856
Nicolò Migliorini <69370563+ghifto@users.noreply.github.com>
2024-10-04 13:58:33 +0200

    Prova salvataggio dati

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const DataStore = require('nedb');
-
-app.listen(3000, () => console.log('listening on port 3000'));
-app.use(express.static('public'));
-app.use(express.json());
-
+const fs = require('fs');
 
 const database = new DataStore('data/database.db');
 database.loadDatabase();
+const content = 'This is the string I want to append to the file.\n';
 
-app.post('/api', (request, response) => {
-    console.log(request.body);
-    response.json({
-        status: "success"
+// Define the file path
+const filePath = 'data/database.db';
+
+function showMessage() {
+    // Display a message when the button is pressed
+    alert("Button has been pressed!");
+    // Append the string to the file (this will add the string at the end of the file)
+    fs.appendFile(filePath, content, (err) => {
+        if (err) {
+            console.error('Error appending to file:', err);
+        } else {
+            console.log('Content has been appended successfully!');
+        }
     });
-});
-
-const timestamp = Date.now();
+}
+*/
